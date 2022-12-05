151f4c568bd2c59f76594ffb732bcb36842cfcc3
Shruti Verma~ <sverma5@ncsu.edu>
2022-12-05 02:30:32 -0500

    Added code for createReminder function

--- commands/reminders.py
+++ commands/reminders.py
@@ -4,8 +4,16 @@ from models import *
 from commands.viewpoints import ViewPoints
 import datetime
 
-
-
+class Reminders:
+    """
+        This class handles the Create Reminder functionality.
+    """
+    def createReminder(self):
+        #Method to create reminder
+        #Fetch pending task list
+        vp = ViewPoints(progress=0.0)
+        pending_tasks = vp.get_list()
+        # print("payload", payload)
 
 
 
