adda7b96827156e2e8572005b2b2446680ff9cca
Shruti Verma~ <sverma5@ncsu.edu>
2022-12-05 02:58:20 -0500

    Added code for createReminder()

--- commands/reminders.py
+++ commands/reminders.py
@@ -12,5 +12,19 @@ class Reminders:
         pending_tasks = vp.get_list()
         # print("payload", payload)
 
+        # dictionary
+        task_details = {}
+        for task in pending_tasks:
+            taskid = task[0]
+            taskname = task[4]
+            taskdate = task[5]
+
+            # I am assuming that taskdate is in str
+            curr_date = datetime.date.today()
+            tomorrow = str(curr_date + datetime.timedelta(days=1))
+
+            if taskdate == tomorrow:
+                task_details[taskid] = taskname
+
 
 
