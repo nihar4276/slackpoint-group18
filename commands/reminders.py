24f7660fe0121e33c95413b6fb50e245294ee81a
Palash <palash10129@gmail.com>
2022-12-05 18:41:06 -0500

    removed unncesary comments from reminders.py

--- commands/reminders.py
+++ commands/reminders.py
@@ -18,19 +18,12 @@ class Reminders:
         tom_tasks=[]
         for taskinfo in listofdict:
             task_text = taskinfo["text"]["text"]
-            #print("task_text ",task_text)
             str_date = re.findall("\d{4}-\d{2}-\d{2}",task_text)[0]
-            #print("str_date :",str_date)
             curr_date = datetime.date.today()
-            #print("curr_date",curr_date)
             task_date=datetime.datetime.strptime(str_date,"%Y-%m-%d").date()
-            #print("task_date",task_date)
             tomorrow = curr_date + datetime.timedelta(days=1)
-            #print("tomorrow :",tomorrow)
             if task_date==tomorrow:
                 tom_tasks.append(task_text)
-
-        # print(tom_tasks)
         return tom_tasks
 
 
